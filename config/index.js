7ec6a59be43dac0d8af2f6dca7f1cb4622911cfd
Gaoswatou <719617315@qq.com>
2020-05-21 10:02:58 +0800

    测试环境默认请求地址调整

--- config/index.js
+++ config/index.js
@@ -1,15 +1,15 @@
 const config = {
   // 开发环境网络请求地址
-  dev: "http://10.168.1.101:8080/wts_server/",
+  dev: 'http://10.168.1.101:8080/wts_server/',
   // 测试环境网络请求地址
-  test: "https://micro-app-test.xmmc.edu.cn/wts_server/",
+  test: 'http://172.43.183.157:8080/wts_server/',
   // 正式环境网络请求地址
-  prod: "https://micro-app.xmmc.edu.cn/wts_server/",
-};
+  prod: 'https://micro-app.xmmc.edu.cn/wts_server/',
+}
 
 const getDefaultUrl = (stage) => {
-  return stage ? config[stage] : config["prod"];
-};
+  return stage ? config[stage] : config['prod']
+}
 module.exports = {
   getDefaultUrl,
-};
+}
